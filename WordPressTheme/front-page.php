c71733d76c1e72401da906dccacf532e70351f17
rei <reiikedzero@gmail.com>
2022-02-10 13:57:17 +0900

    0210

--- WordPressTheme/front-page.php
+++ WordPressTheme/front-page.php
@@ -1,5 +1,44 @@
 <?php get_header(); ?>
 
+<main>
+    <section class="p-success l-success">
+        <div class="l-inner succes__inner">
 
+            <h2 class="p-success__title c-section-title"></h2>
+
+            <ul class="p-success__items p-card-list">
+                <li class="p-card-list__item p-case-card">
+                    <h3 class="p-case-card__title">TOEFL iBT 100点を突破してコロンビア大学大学院に進学できました！</h3>
+                    <figure class="p-case-card__img"><img src="<?php echo get_template_directory_uri() ?>/assets/img/top/case01.png" alt="toefl ibt 100点を突破してコロンビア大学大学院に進学できました"></figure>
+                    <div class="p-case-card__body">
+                        <span class="p-case-card__category">会社員</span>
+                        <span class="p-case-card__name">会社員</span>
+                        <p class="p-case-card__up">3ヶ月でTOEFL80→108点</p>
+                    </div>
+                </li>
+                <li class="p-card-list__item p-case-card">
+                    <h3 class="p-case-card__title">TOEFL iBT 100点を突破してコロンビア大学大学院に進学できました！</h3>
+                    <figure class="p-case-card__img"><img src="<?php echo get_template_directory_uri() ?>/assets/img/top/case01.png" alt="toefl ibt 100点を突破してコロンビア大学大学院に進学できました"></figure>
+                    <div class="p-case-card__body">
+                        <span class="p-case-card__category">会社員</span>
+                        <span class="p-case-card__name">会社員</span>
+                        <p class="p-case-card__up">3ヶ月でTOEFL80→108点</p>
+                    </div>
+                </li>
+                <li class="p-card-list__item p-case-card">
+                    <h3 class="p-case-card__title">TOEFL iBT 100点を突破してコロンビア大学大学院に進学できました！</h3>
+                    <figure class="p-case-card__img"><img src="<?php echo get_template_directory_uri() ?>/assets/img/top/case01.png" alt="toefl ibt 100点を突破してコロンビア大学大学院に進学できました"></figure>
+                    <div class="p-case-card__body">
+                        <span class="p-case-card__category">会社員</span>
+                        <span class="p-case-card__name">会社員</span>
+                        <p class="p-case-card__up">3ヶ月でTOEFL80→108点</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+
+    </section>
+
+</main>
 
 <?php get_footer(); ?>
